3dc2705c7a2db4c1e1991d3a89222a8350edbcd7
bluesatyr <shawnevans.music@gmail.com>
2021-02-13 15:04:41 -0500

    add html template

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,92 +1,69 @@
-// create the about section
 
-/*
-const generateAbout = aboutText => {
-    if (!aboutText) {
-        return '';
-    }
-    return `
-      <section class="my-3" id="about">
-        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-        <p>${aboutText}</p>
-      </section>
-    `;
-};
-*/
 
-// generateCards()
-const generateProjects = projectsArr => {
-  return `
-    <section class="my-3" id="portfolio">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-      <div class="flex-row justify-space-between">
-      ${projectsArr
-        .filter(({ feature }) => feature)
-        .map(({ name, description, languages, link }) => {
-          return `
-          <div class="col-12 mb-2 bg-dark text-light p-3">
-            <h3 class="portfolio-item-title text-light">${name}</h3>
-            <h5 class="portfolio-languages">
-              Built With:
-              ${languages.join(', ')}
-            </h5>
-            <p>${description}</p>
-            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-          </div>
-        `;
-        })
-        .join('')}
+const generateCards = teamData => {
+  // text of all cards to be added to page html
+  let cardsHTML = '';
+  for (let i = 0; i < teamData.length; i++) {
+    const teamMember = teamData[i]
+    // get role specific info for cards
+    switch (teamMember.getRole) {
+      case 'Manager':
+        var roleInfo = `Office ${teamMember.getOffice}`;
+        var roleIcon = "fas fa-crown";
+        break;
+      case 'Engineer':
+        var roleInfo = `GitHub: https://github.com/${teamMember.getGithub()}/`;
+        var roleIcon = "fas fa-laptop-code";
+        break;
+      case 'Intern':
+        var roleInfo = `School: ${teamMember.getSchool()}`;
+        var roleIcon = "fas fa-user-graduate";
+    };
+    // create card using team data array.
+    const card = `
+  <div class="card">
+    <div class="employee">
+        <h2>${teamMember.getName()}</h2>
+        <i class="${roleIcon}"></i><span class="role">${teamMember.getRole()}</span>
+    </div>
+    <div class="employee-info">
+      <div class="employee-id">ID: ${teamMember.getId()}</div>
+      <div class="email">${teamMember.getEmail()}</div>
+      <div class="role-info">${roleInfo}</div>
+    </div>
+  </div>`;
 
-      ${projectsArr
-        .filter(({ feature }) => !feature)
-        .map(({ name, description, languages, link }) => {
-          return `
-          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-            <h3 class="portfolio-item-title text-light">${name}</h3>
-            <h5 class="portfolio-languages">
-              Built With:
-              ${languages.join(', ')}
-            </h5>
-            <p>${description}</p>
-            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-          </div>
-        `;
-        })
-        .join('')}
-      </div>
-    </section>
-  `;
-};
+    cardsHTML += card;
+  };
 
-module.exports = templateData => {
-  // destructure page data by section
-  const { projects, about, ...header } = templateData;
+  return cardsHTML;
+}
 
+//
+const generateHTML = (teamData) => {
   return `
   <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
-        <title>Our Team</title>
-        <!-- Font Awesome -->
-        <script src="https://kit.fontawesome.com/ab5ed30506.js"></script>
-        <link rel="stylesheet" href="./test.css"> 
-      </head>
-      <body>
-        <header>
-            <div><h1>Our Team</h1></div>
-        </header>
-        <main>
-      
-        ${generateCards()}
-
-
-        </main>
-    
-      </body>
-      </html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+      <title>Our Team</title>
+      <!-- Font Awesome -->
+      <script src="https://kit.fontawesome.com/ab5ed30506.js"></script>
+      <link rel="stylesheet" href="./test.css"> 
+  </head>
+  <body>
+      <header>
+          <div><h1>Our Team</h1></div>
+      </header>
+      <main>
+          <div class="flex-container">
+            ${generateCards(teamData)}
+          </div>
+      </main>
+  </body>
+  </html>
   `;
 };
 
-//module.exports = generatePage;
+module.exports = generateHTML;
